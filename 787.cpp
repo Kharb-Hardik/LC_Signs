36d5f840aa7a37c545e2836b675c9729013fd5f4
Kharb-Hardik <hardik.kharb05@gmail.com>
2025-03-13 23:13:12 +0530

    Bellman Ford can help get The the K nodes Condition in Graphs

---
+++ 787.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+    public:
+        int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+            vector<int> dist(n, INT_MAX);
+            dist[src] = 0;
+    
+            for (int i = 0; i <= k; i++) {
+                vector<int> temp = dist; 
+                for (auto& edge : flights) {
+                    int u = edge[0], v = edge[1], w = edge[2];
+                    if (dist[u] != INT_MAX && dist[u] + w < temp[v]) {
+                        temp[v] = dist[u] + w;
+                    }
+                }
+                dist = temp;
+            }
+    
+            return dist[dst] == INT_MAX ? -1 : dist[dst];
+        }
+    };
+    
